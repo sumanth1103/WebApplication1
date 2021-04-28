5bcb3fba4a6b0e54c34a44045ea837b25814091d
Reddipalli <899167@cognizant.com>
2021-04-28 20:32:19 +0530

    Add project files.

---
+++ WebApplication1/Models/UserClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class UserClass
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string Designation { get; set; }
+        public string City { get; set; }
+        public string Salary { get; set; }
+    }
+}
